b48d769277ce70197b1c1c453f5ac466a30d7fee
Alex Monteverde <55471655+Amonteverde04@users.noreply.github.com>
2023-04-05 00:02:04 -0400

    Fixing html file styles, getting rid of picture of cats to simplify the 'blog'. Updating the script to be data driven. Updating css. More to come.

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// await Server call for ad data...
-
 // We can refactor below.
-// This just demonstrates that it is possible. In the future the response object can have a list of multiple ads.
-
-// Image
-/*
-let ResponseObj = {
-  AdId: "122",
-  AdType: "img",
-  src: "https://cdn.dribbble.com/userupload/5971840/file/original-2cc663fc3ea9857d25a5195653dee36e.jpg?compress=1&resize=752x",
-  AdTypeParams:
-    "style='max-height: 300px; max-width: auto; display: block; margin: auto;'",
-  RedirectLink: "https://www.google.com/",
-};
-*/
-
-// Video
-let ResponseObj = {
-  AdId: "123",
-  AdType: "video",
-  src: "https://cdn.dribbble.com/userupload/5973349/file/original-f9a8e6a46c0bf919b14ade317b8c2af0.mp4",
-  AdTypeParams:
-    "playInline autoplay muted loop style='max-height: 300px; max-width: auto; display: block; margin: auto; cursor: pointer;'",
-  RedirectLink: "https://www.google.com/",
-};
-
-// Add the advertisement after every p tag. With further development and testing, we can scale to websites of all kinds.
-// If we had a list of ads, we could iterate through and add each's redirect link to each image or video element.
-const body = document.getElementsByTagName("p");
-for (let i = 0; i < body.length; i++) {
-  body[i].insertAdjacentHTML(
-    "afterend",
-    `<${ResponseObj.AdType} src=${ResponseObj.src} ${ResponseObj.AdTypeParams} class=${ResponseObj.AdId} />`
+// This just demonstrates that it is possible. In the future we can iterate through different ads instead of only using the first ad object.
+async function GetAdvertisements() {
+  let ResponseArray = [];
+  const response = await fetch(
+    "http://localhost:3000/advertisements/get-ad-data",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    }
   );
-}
 
-const ads = document.getElementsByClassName(ResponseObj.AdId);
-for (let i = 0; i < body.length; i++) {
-  ads[i].addEventListener("click", () => {
-    // make async call to server to update click for the image or video using it's id then we redirect.
-    window.location.href = ResponseObj.RedirectLink;
-  });
+  const AdvertisementList = await response.json();
+  const AdvertisementListLength = Object.keys(AdvertisementList).length;
+
+  for (let i = 0; i < AdvertisementListLength; i++) {
+    ResponseArray.push({
+      token: AdvertisementList[i].token,
+      mediaType: AdvertisementList[i].mediaType,
+      redirectLink: AdvertisementList[i].redirectLink,
+      src: AdvertisementList[i].src,
+      mediaTypeParams: AdvertisementList[i].mediaTypeParams,
+    });
+  }
+
+  // Add the advertisement after every p tag. With further development and testing, we can scale to tags of all kinds.
+  const body = document.getElementsByTagName("p");
+  for (let i = 0; i < body.length; i++) {
+    // using i here works because as of 4/4/2023, DB has two paragraph tags and two ads to display. In the future there may be more or less tags for us to put ads on.
+    body[i].insertAdjacentHTML(
+      "afterend",
+      `<${ResponseArray[i].mediaType} src=${ResponseArray[i].src} ${ResponseArray[i].mediaTypeParams} class=${ResponseArray[i].token} />`
+    );
+
+    // Look for the ad that was just added. Add redirect on click event listener
+    let ads = document.getElementsByClassName(ResponseArray[i].token);
+    ads[0].addEventListener("click", () => {
+      window.location.href = ResponseArray[i].redirectLink;
+    });
+  }
 }
+GetAdvertisements();
